03d89d3bc81639b609b680e0d8f37041dd14df16
clintt-09 <aisosaclinton09@gmail.com>
2024-05-29 14:16:13 +0100

    third commit

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 let counter= 0
 let count= 0;
+let count2= 0;
+const mobile_darkMode = document.querySelector('#svg-dark');
+const about_components_bg = document.querySelector('.about-components');
+const about_components = document.querySelectorAll('.flex-item');
+
+mobile_darkMode.innerHTML = `<path d="M3.32031 11.6835C3.32031 16.6541 7.34975 20.6835 12.3203 20.6835C16.1075 20.6835 19.3483 18.3443 20.6768 15.032C19.6402 15.4486 18.5059 15.6834 17.3203 15.6834C12.3497 15.6834 8.32031 11.654 8.32031 6.68342C8.32031 5.50338 8.55165 4.36259 8.96453 3.32996C5.65605 4.66028 3.32031 7.89912 3.32031 11.6835Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>`;
 
 hamburger.addEventListener('click', () => {
     counter++;
@@ -16,14 +22,7 @@ hamburger.addEventListener('click', () => {
 
 
 
-
-
-
-
-
-
-
-const darkModeBtn = document.querySelector('#dark');
+const darkModeBtn = document.querySelector('.dark');
 const lightModeStr = './assets/sun-svgrepo-com.svg';
 const darkModeStr = './assets/moon-svgrepo-com.svg';
 
@@ -33,13 +32,34 @@ function darkMode(){
         darkModeBtn.setAttribute('src', lightModeStr);
         document.querySelector('body').style.backgroundColor = '#000000';
         document.querySelector('body').style.color = '#ffffff'
+        about_components_bg.style.backgroundColor = 'rgba(29, 31, 29)';
+
         return;
     }else{
         darkModeBtn.setAttribute('src', darkModeStr);
         document.querySelector('body').style.backgroundColor = '#ffffff';
-        document.querySelector('body').style.color = '#000000'
+        document.querySelector('body').style.color = '#000000';
+        about_components_bg.style.backgroundColor = '#f7f7f7'
     }
 }
 
 
 darkModeBtn.addEventListener('click', darkMode)
+mobile_darkMode.addEventListener('click', ()=>{
+    count2++;
+    if (count2 % 2 !== 0){
+        console.log(count2);
+        mobile_darkMode.innerHTML = `
+        <path d="M12 3V4M12 20V21M4 12H3M6.31412 6.31412L5.5 5.5M17.6859 6.31412L18.5 5.5M6.31412 17.69L5.5 18.5001M17.6859 17.69L18.5 18.5001M21 12H20M16 12C16 14.2091 14.2091 16 12 16C9.79086 16 8 14.2091 8 12C8 9.79086 9.79086 8 12 8C14.2091 8 16 9.79086 16 12Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        `;
+        document.querySelector('body').style.backgroundColor = '#000000';
+        document.querySelector('body').style.color = '#ffffff'
+        return;
+    }else{
+        mobile_darkMode.innerHTML = `
+        <path d="M3.32031 11.6835C3.32031 16.6541 7.34975 20.6835 12.3203 20.6835C16.1075 20.6835 19.3483 18.3443 20.6768 15.032C19.6402 15.4486 18.5059 15.6834 17.3203 15.6834C12.3497 15.6834 8.32031 11.654 8.32031 6.68342C8.32031 5.50338 8.55165 4.36259 8.96453 3.32996C5.65605 4.66028 3.32031 7.89912 3.32031 11.6835Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        `;
+        document.querySelector('body').style.backgroundColor = '#ffffff';
+        document.querySelector('body').style.color = '#000000'
+    }
+});
